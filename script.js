d0d7fa94dd36e585e635a0d0768e57ab30153d31
errEstiak <estiak35-2998@diu.edu.bd>
2023-06-08 22:40:06 +0600

    making the again button workable & resetting all values to default

--- script.js
+++ script.js
@@ -11,10 +11,25 @@ console.log(document.querySelector('.guess').value);
 // ** implement too high / too low concept
 // ** working on score
 
-const secretNumber = Math.trunc(Math.random() * 20)+1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20)+1;
 
 let score = 20;
+
+
+document.querySelector('.again').addEventListener('click', function(){
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20)+1;
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem'
+})
+
+
 
 document.querySelector('.check').addEventListener('click', function(){
   const guess = Number((document.querySelector('.guess').value));
@@ -25,6 +40,10 @@ document.querySelector('.check').addEventListener('click', function(){
   }
   else if(guess === secretNumber){
     document.querySelector('.message').textContent = '🎉 Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem'
   }
   else if(guess > secretNumber){
     if(score > 1){
